018646fa73b3d630cccd6891170c176d18e45cce
Brian <enriquebrian01@gmail.com>
2022-10-13 16:31:52 -0400

    added effects to player and monster

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour {
     // Start is called before the first frame update
+
+    public ParticleSystem ps;
+    void Start() {
+        var emissionModule = ps.emission;
+        emissionModule.enabled = true;
+    }
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Monster")) {
             Debug.Log("you died");
